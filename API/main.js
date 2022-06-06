b975277df71dbcef320a450cb11e2e22b59e96e7
OctyMapache <ooctyy1@gmail.com>
2022-06-06 04:04:29 -0300

    revisar error

---
+++ API/main.js
@@ -0,0 +1,11 @@
+async function dolar_hoy(){
+
+    const dolar = await fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
+
+    const datos = await dolar.json()
+
+    datos.forEach((element, index) => {
+        console.log(index + '-' + element.casa.nombre + '- Compra:' + element.casa.nombre + '- Venta:'= element.casa.nombre)       
+    });
+}
+dolar_hoy()
